1da91580754942aee59424f64a99e650775c033f
gabrieltomita@hotmail.com <gabrieltomita@hotmail.com>
2026-04-16 20:59:46 -0300

    feature: implementado a validação de espaço em branco

--- src/service/ValidadorSenha.js
+++ src/service/ValidadorSenha.js
@@ -1,11 +1,12 @@
 function validarSenha(senha) {
   if (senha.length < 8) return false;
+  if (senha.includes(" ")) return false;
 
   const temMaiuscula = /[A-Z]/.test(senha);
   const temMinuscula = /[a-z]/.test(senha);
   const temNumero = /[0-9]/.test(senha);
   const temEspecial = /[!@#$%^&*]/.test(senha);
 
-return temMaiuscula && temMinuscula && temNumero && temEspecial;
+  return temMaiuscula && temMinuscula && temNumero && temEspecial;
 }
 module.exports = { validarSenha };
