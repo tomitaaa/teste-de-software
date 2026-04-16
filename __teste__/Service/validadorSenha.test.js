4b87e830d097372e62efb58aa9d72af86e0d0b03
gabrieltomita@hotmail.com <gabrieltomita@hotmail.com>
2026-04-16 20:29:28 -0300

    teste: não pode aceitar a senha sem uma letra maiuscula

--- __teste__/Service/validadorSenha.test.js
+++ __teste__/Service/validadorSenha.test.js
@@ -1,9 +1,11 @@
 const { validarSenha } = require("../../src/service/ValidadorSenha");
 
 describe("Verifcador de Senhas", () => {
-
   test("não irá aceitar uma senha com menos de 8 caracteres", () => {
     expect(validarSenha("Ab1!")).toBe(false);
   });
 
+  test("não irá aceitar uma senha sem letra maiúscula", () => {
+    expect(validarSenha("ifprpvai")).toBe(false);
+  });
 });
