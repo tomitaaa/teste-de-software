bd9859b1a5f6214dc32e4bf26b2c062fd28cf508
gabrieltomita@hotmail.com <gabrieltomita@hotmail.com>
2026-04-16 20:46:52 -0300

    feature: implementa a validação de letra minuscula

--- src/service/ValidadorSenha.js
+++ src/service/ValidadorSenha.js
@@ -2,7 +2,8 @@ function validarSenha(senha) {
   if (senha.length < 8) return false;
 
   const temMaiuscula = /[A-Z]/.test(senha);
+  const temMinuscula = /[a-z]/.test(senha);
 
-  return temMaiuscula;
+  return temMaiuscula && temMinuscula;
 }
 module.exports = { validarSenha };
