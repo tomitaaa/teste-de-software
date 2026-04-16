1972ae33f9a4a17ac3bfb9a379653cf464e55e56
gabrieltomita@hotmail.com <gabrieltomita@hotmail.com>
2026-04-16 20:54:19 -0300

    feature: implementado a validação de caracteres especiais

--- src/service/ValidadorSenha.js
+++ src/service/ValidadorSenha.js
@@ -4,7 +4,8 @@ function validarSenha(senha) {
   const temMaiuscula = /[A-Z]/.test(senha);
   const temMinuscula = /[a-z]/.test(senha);
   const temNumero = /[0-9]/.test(senha);
+  const temEspecial = /[!@#$%^&*]/.test(senha);
 
-  return temMaiuscula && temMinuscula && temNumero;
+return temMaiuscula && temMinuscula && temNumero && temEspecial;
 }
 module.exports = { validarSenha };
